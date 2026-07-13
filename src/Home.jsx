25751570e83d7a80415a6265ea6e606b9c7591b1
0952460525-lab <0952460525@aitec.edu.ec>
2026-07-13 09:12:58 -0500

    Create Home component to display character list

---
+++ src/Home.jsx
@@ -0,0 +1,10 @@
+import CharacterList from '../components/CharacterList';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Personajes de Rick and Morty</h2>
+      <CharacterList />
+    </div>
+  );
+}
